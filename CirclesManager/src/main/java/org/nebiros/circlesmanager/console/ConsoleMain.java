936bb11d439b7bd39e29546f4384e99d61209f36
Nebiros <altur13.osu@gmail.com>
2020-08-31 15:01:37 -0300

    Basic repl skeleton

--- CirclesManager/src/main/java/org/nebiros/circlesmanager/console/ConsoleMain.java
+++ CirclesManager/src/main/java/org/nebiros/circlesmanager/console/ConsoleMain.java
@@ -1,9 +1,32 @@
 package org.nebiros.circlesmanager.console;
 
+import java.util.Scanner;
+
 public class ConsoleMain
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+        repl();
+    }
+
+    public static void repl()
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            System.out.print("> ");
+            String cmd = scanner.nextLine();
+            String[] cmd_parts = cmd.split(" ");
+            String cmd_norm = cmd_parts[0].toLowerCase();
+
+            switch (cmd_norm)
+            {
+                case "exit":
+                case "q":
+                    return;
+                default:
+                    System.out.println("Unknown command " + cmd_parts[0]);
+            }
+        }
     }
 }
